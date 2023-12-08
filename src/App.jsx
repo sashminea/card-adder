b6b2c6bca0c26e1f9933c6ef93e3b3c7dd9091e6
Sashmin Adhikari <sashminadhikari@gmail.com>
2023-12-08 22:42:37 +0545

    Added card adder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [cards, setCards] = useState([]);
+
+  const handleAddCard = () => {
+
+    setCards([...cards, <Card key={cards.length} />])
+
+  };
 
   return (
     <>
+      <button className="addButton" onClick={handleAddCard}>+</button>
 
+      <div className='card-container'>
+        {cards.map((card, index) => (
+
+          <div key={index}>{card}</div>
+        ))}
+      </div>
     </>
   )
 }
